9673b6d375cda0c69a4cbae066029d09a18e8b26
叮咚叮咚 <15069260+ddxidong@user.noreply.gitee.com>
2024-11-30 21:50:49 +0800

    feat: 支持GUI界面

--- LessonRecord.py
+++ LessonRecord.py
@@ -1,30 +1,63 @@
-import json,os
+import json,os,tkinter
+from tkinter import ttk
 
-# 从文件中读取JSON字符串
+top = tkinter.Tk()
+
 with open('LessonRecord1.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 with open('setting.json', 'r', encoding='utf-8') as json_file:
     setting = json.load(json_file)
 
-print("欢迎使用记录器v"+setting['version']+"！")
-print("[info] json文件加载完毕！\n")
-subject=input("请输入科目名称：")
-if subject in str(data):
-    time=int(input("请输入时间："))
+subjects = []
+for record in data["records"]:
+    subjects.append(record["subject"])
+
+
+top.title("记录器v"+setting['version'])
+top.geometry("300x150")
+top.resizable(0,0)
+
+label = tkinter.Label(top, text="欢迎使用记录器v"+setting['version']+"！")
+label.grid(row=0, columnspan=2)# 显示欢迎信息
+
+xVariable = tkinter.StringVar()     # #创建变量，便于取值
+ 
+com = ttk.Combobox(top, textvariable=xVariable)     # #创建下拉菜单
+com.grid(row=1, column=0, columnspan=2)  # #将下拉菜单绑定到窗体
+com["value"] = subjects    # #给下拉菜单设定值
+com.current(0)    #设定下拉菜单的默认值
+sub = com.get()
+
+Entry = tkinter.Entry(top, show=None)
+Entry.grid(row=2, column=0, columnspan=2)
+
+def record():
+    global sub,data
     for record in data["records"]:
-        if record["subject"] == subject:
-            # 找到后，向lessons数组添加新项
-            record["lessons"].append(time)
-            break 
-    print("记录成功！")
-else:
-    print("科目不存在！")
+        if record["subject"] == sub:
+            try:
+                time = int(Entry.get())
+            except:
+                tkinter.messagebox.showinfo(title='错误提示',
+            		message='您输入的数据不合法，请检查是否为阿拉伯数字、是否为空！') 
+                break
+                
+            record["lessons"].append(int(Entry.get()))
+            print("[info] 记录成功！")
+            break
 
-file_name=data['name']+'.json'
-with open(file_name, 'w', encoding='utf-8') as json_file:
-    json.dump(data, json_file, ensure_ascii=False, indent=4)
+def save():
+    file_name=data['name']+'.json'
+    with open(file_name, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+    print("\n[info] json文件保存完毕！")
 
-print("\n[info] json文件保存完毕！")
-print("按任意键退出。")
 
-no_ans=input()
+
+record_button = tkinter.Button(top, text="记录", command=record) 
+save_button = tkinter.Button(top, text="保存", command=save)
+record_button.grid(row=3, column=0)
+save_button.grid(row=3, column=1)
+
+
+top.mainloop()
