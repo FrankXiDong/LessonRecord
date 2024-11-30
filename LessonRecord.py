666f8b45fd8e487c0fd8b4ee0daf2b3a5642b4cf
叮咚叮咚 <15069260+ddxidong@user.noreply.gitee.com>
2024-11-30 18:01:31 +0800

    feat: 优化版本号的使用

--- LessonRecord.py
+++ LessonRecord.py
@@ -3,8 +3,11 @@ import json,os
 # 从文件中读取JSON字符串
 with open('LessonRecord1.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
+with open('setting.json', 'r', encoding='utf-8') as json_file:
+    setting = json.load(json_file)
 
-print("（记录器0.1.0）json文件加载完毕！")
+print("欢迎使用记录器v"+setting['version']+"！")
+print("[info] json文件加载完毕！\n")
 subject=input("请输入科目名称：")
 if subject in str(data):
     time=int(input("请输入时间："))
@@ -17,7 +20,11 @@ if subject in str(data):
 else:
     print("科目不存在！")
 
-
 file_name=data['name']+'.json'
 with open(file_name, 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+print("\n[info] json文件保存完毕！")
+print("按任意键退出。")
+
+no_ans=input()
